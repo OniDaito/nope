716d540dc7aa14124e1457a9451ba3bf306efd47
Benjamin Blundell <me@benjamin.computer>
2022-09-22 08:50:45 +0100

    Split out items

--- data/loader.py
+++ data/loader.py
@@ -28,72 +28,8 @@ import math
 from tqdm import tqdm
 from enum import Enum
 from util.math import Points, Point, Mask, Trans, VecRot, Stretch
+from item import LoaderItem, ItemSimulated, ItemType
 
-ItemType = Enum("SetType", "SIMULATED FITSIMAGE")
-
-
-class LoaderItem:
-    """The item returned by any of the various Loaders.
-    This is the base class, expanded upon below."""
-
-    def __init__(self):
-        self.type = ItemType.SIMULATED
-
-    def unpack(self):
-        assert False
-        return []
-
-
-class ItemSimulated(LoaderItem):
-    """ The Simulated items returned by the basic loader."""
-
-    def __init__(
-        self, points: Points, mask: Mask, angle_axis: VecRot, trans: Trans, stretch: Stretch, sigma: float
-    ):
-        """
-        Create our ItemSimulated.
-
-        Parameters
-        ----------
-        points : Points
-           The points that make up this datum.
-        mask : Mask
-            The mask for the points.
-        angle_axis : VecRot
-            The rotation of this datum.
-        trans : Trans
-            The translation of this datum.
-        sigma : float
-            The sigma this datum should be rendered with.
-
-        Returns
-        -------
-        self
-        """
-        super().__init__()
-        self.type = ItemType.SIMULATED
-        self.points = points
-        self.mask = mask
-        self.angle_axis = angle_axis
-        self.trans = trans
-        self.stretch = stretch
-        self.sigma = sigma
-
-    def unpack(self) -> tuple:
-        """
-        Unpack the item, return a tuple.
-
-        Parameters
-        ----------
-        None
-
-        Returns
-        -------
-        tuple
-            The item as a tuple in the following order:
-            points, mask, rotation, translation, sigma
-        """
-        return (self.points, self.mask, self.angle_axis, self.trans, self.sigma)
 
 
 def sort_models(tup):
